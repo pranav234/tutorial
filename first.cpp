886e8ed072388db4f0792bca0b97a0b2127a927b
pranav mahajan <pranav234@pranavs-MacBook-Air.local>
2019-02-10 16:37:05 +0530

    Fixed Multiply

--- first.cpp
+++ first.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 
-void multiply(int a , int b) {
-	return a*b;
-}
 int main() {
 cout << "Hello World" << endl;
 return 0;
+}
+
+void multiply(int a , int b) {
+        return a*b;
 }
